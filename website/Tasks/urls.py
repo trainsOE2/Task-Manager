484850488a2063c36f4492fe54ce5dca33048df4
Megha Singh <Megha Singh>
2016-12-20 22:25:18 +0530

    changed from job_id to pk

--- website/Tasks/urls.py
+++ website/Tasks/urls.py
@@ -6,7 +6,7 @@ app_name = 'Tasks'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
-    url(r'^(?P<job_id>[0-9]+)/$', views.DetailView.as_view(), name='detail' ),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail' ),
 
 
 ]
