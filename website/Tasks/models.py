b76ca19207a581a04e3f974056837f64fb988d58
Megha Singh <Megha Singh>
2016-12-20 22:37:24 +0530

    previous models.py restored

--- website/Tasks/models.py
+++ website/Tasks/models.py
@@ -13,6 +13,7 @@ class Job(models.Model):
 class Goal(models.Model):
     identity = models.ForeignKey(Job, on_delete = models.CASCADE)
     illustrate = models.CharField(max_length = 400)
+    is_achieved = models.BooleanField(default = False)
 
     def __str__(self):
         return self.illustrate
