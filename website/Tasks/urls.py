32abd7255281bcfda73e162fe0cbec148112cd39
Megha Singh <Megha Singh>
2016-12-20 21:55:09 +0530

    using generic

--- website/Tasks/urls.py
+++ website/Tasks/urls.py
@@ -4,11 +4,9 @@ from . import views
 app_name = 'Tasks'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    url(r'^(?P<job_id>[0-9]+)/$', views.detail, name='detail' ),
-
-    url(r'^(?P<job_id>[0-9]+)/achieved/$', views.achieved, name='achieved' ),
+    url(r'^(?P<job_id>[0-9]+)/$', views.DetailView.as_view(), name='detail' ),
 
 
 ]
